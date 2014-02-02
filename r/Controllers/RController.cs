26757a65f42dd0b14a9e8d295c6af9323cfcd330
Michael <mikevh@gmail.com>
2014-02-02 10:23:38 -0800

    new project

---
+++ r/Controllers/RController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace r.Controllers
+{
+    public class RController : Controller
+    {
+        //
+        // GET: /R/
+        public ActionResult Index()
+        {
+            return RedirectPermanent("http://twitter.com");
+
+            //return View();
+        }
+	}
+}
